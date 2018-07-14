ebdf22c9ad5afe9e9588122fddcbde3d091ad25c
Pablo Argueta <pabloargueta27@gmail.com>
2018-07-14 15:48:27 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,6 +1,7 @@
 # Write your code here.
 def dictionary
 dictionary = {
+<<<<<<< HEAD
   "hello" => "hi",
   "to" => "2",
   "two"=> "2",
@@ -11,12 +12,25 @@ dictionary = {
   "you" => "u",
   "at" => "@",
   "and"=> "&"
+=======
+  "hello": "hi",
+  "to": "2",
+  "two": "2",
+  "too": "2",
+  "for": "4",
+  "four": "4",
+  "be": "b",
+  "you": "u",
+  "at": "@",
+  "and": "&"
+>>>>>>> b31b63ba5e92a0afc2e86c517d3ae1f3d01cfee2
 }
 
 end
 
 def word_substituter(tweet_string)
   
+<<<<<<< HEAD
   tweet_array = tweet_string.split(" ")
   
   tweet_array.collect{ |word|
@@ -51,4 +65,14 @@ def shortened_tweet_truncator(tweet)
   else
     tweet
   end
+=======
+  tweet_string.split.collect do |word|
+    if dictionary.keys.include?(word.downcase)
+      word = dictionary[word.downcase.to_sym]
+    else
+      word
+    end
+  end.join(" ")
+
+>>>>>>> b31b63ba5e92a0afc2e86c517d3ae1f3d01cfee2
 end
